ad2dd8b9e3dd1702618cb66638805b80b852e217
Thomas Scholtes <thomas-scholtes@gmx.de>
2014-10-30 16:33:57 +0100

    Add services

---
+++ client/services.ts
@@ -0,0 +1,67 @@
+/// <reference path="../typings/underscore/underscore.d.ts" />
+import _ = require('underscore');
+var map = _.map;
+
+
+export class ServiceProvider {
+
+    provide(name: string, s: Service<any>) {
+        this.services[name] = s;
+    }
+
+    get(name: string) {
+        var instance = this.instances[name]
+        if (typeof instance == 'undefined')
+            instance = this.resolve(name);
+        return instance;
+    }
+
+    private resolve(name: string) {
+        var service = this.services[name]
+        if (typeof service == 'undefined')
+            throw Error('Service ' + name + 'is not provided');
+        if (this.resolving.indexOf(name) >= 0)
+            throw Error('Circular dependency in requiring ' + name);
+
+        this.resolving.push(name);
+        var deps = map(service.deps, (d) => this.get(d));
+        var instance = service.init(deps);
+        this.resolving.pop();
+        return instance;
+    }
+
+    private services: {[name: string]: Service<any>} = {};
+    private instances: {[name: string]: any} = {};
+    private resolving: string[] = [];
+}
+
+
+export function service<T>(init: (...any) => T)
+export function service<T>(deps: string[], init: (...any) => T)
+export function service<T>(deps, init?) {
+    if (typeof init == 'undefined') {
+        init = deps;
+        deps = [];
+    }
+    return new Service<T>(deps, init)
+}
+
+
+export class Service<T> {
+    constructor(deps: string[], init: (...any) => T) {
+        this.deps = deps;
+        this._init = init;
+    }
+
+    deps: string[];
+
+    init(args: any[]): T {
+        if (!this.initiated)
+            this.instance = this._init.apply(undefined, args);
+        return this.instance;
+    }
+
+    private initiated: boolean = false;
+    private instance: T;
+    private _init: (...any) => T;
+}
---
+++ test/client/services.coffee
@@ -0,0 +1,19 @@
+define ['services', 'support']
+, (
+  {ServiceProvider, service},
+  {should}
+)->
+
+  describe.only 'services', ->
+
+    Given 'a provider', -> new ServiceProvider
+
+    When -> @provider.provide('a', service -> 'no deps')
+    When 'service', -> @provider.get('a')
+    Then 'service', should.equal('no deps')
+
+    When -> @provider.provide('a', service ['b', 'c'], (b, c)-> [b,c])
+    When -> @provider.provide('b', service -> 'b')
+    When -> @provider.provide('c', service -> 'c')
+    When 'service', -> @provider.get('a')
+    Then 'service', should.deep.equal(['b', 'c'])
